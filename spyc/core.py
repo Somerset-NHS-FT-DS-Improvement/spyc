cfbfd2215c2cd052882520b12d33d8a68b643cbc
Benjamin Holdsworth <Benjamin.Holdsworth@SomersetFT.nhs.uk>
2025-01-02 08:22:46 +0000

    seperating graphing functionality

--- spyc/core.py
+++ spyc/core.py
@@ -1,9 +1,11 @@
 import numpy as np
 import pandas as pd
+import warnings
+import plotly.graph_objects as go
+
 from .control_rules import _rules_func
 from .seasonal_formatting import _add_seasonal_column, _group_data_by_season, _data_points_per_season
 from .helpers import convert_to_timestamp
-from .plotly_chart import plotly_chart, combine_figures
 
 
 class SPC:
@@ -11,33 +13,43 @@ class SPC:
     def __init__(self,
                  data_in,
                  target_col,
+                 seasonal=None,
                  fix_control_start_dt=None,
-                 fix_control_end_dt=None,
-                 seasonal=None):
+                 fix_control_end_dt=None):
 
         """
-        Statistical Process Control automation, which can generalise for any custom/default chart type (defined using
-        the spc_calc_func parameter).
-        Seasonal adjustments (control defined for each season) can be set by using the seasonal parameter, if present.
-        Trends are not automatically handled and should ideally be removed prior to feeding in dataset, if present.
-        Control lines can be re-calculated following a process change, using the add_process_change_date() method.
+        A tool for streamlining Statistical Process Control (SPC) analytics.
+        
+        Functionality:
+            - Generalisable for custom/default control chart types.
+            - Allows for seasonal patterns to be captured (daily/weekly/monthly/quarterly). Note, trends are not 
+            automatically handled and should ideally be removed prior to feeding in dataset, if present
+            - Allows for control lines to be re-calculated following a process change.
 
-        - data_in (Pandas.DataFrame): Datetime index with frequency attribute.
-        - target_col (str): Must exist in data_in.
-        - fix_control_start_dt (str): If None, reverts to start of data/start of previous value in process_change_dates.
-        - fix_control_end_dt (str): If None, reverts to end of data/end of next value in process_change_dates.
-        - seasonal (int): Time steps for repeating seasonal pattern (i.e., 24 for hourly data, 7 for daily etc...)
+        Inputs:
+            - data_in (pd.DataFrame): The input data containing the process measurements, with DateTime index.
+            - target_col (str): The column name in the data containing the target measurements.
+            - seasonal (int, optional): The number of time steps for repeating seasonal patterns (e.g., 24 for hourly
+              data, 7 for daily data). Defaults to None.
+            - fix_control_start_dt (str): If None, reverts to start of data/start of previous value in
+            process_change_dates.
+            - fix_control_end_dt (str): If None, reverts to end of data/end of next value in process_change_dates.
         """
 
-        self.spc_outputs_dict = {}
+        self.control_line_dates = None
+        self.seasonal_periods = None
+        self.seasonal = None
+        self.target_col = None
+        self.data_in = None
         self.data_in = data_in.copy()
         self.target_col = target_col
+        self.seasonal = seasonal
 
         # If values not set, default to start/end dates of data_in
         self.fix_control_start_dt = fix_control_start_dt if fix_control_start_dt is not None \
             else self.data_in.index.min()
-        self.fix_control_end_dt = fix_control_end_dt if fix_control_end_dt is not None else self.data_in.index.max()
-        self.seasonal = seasonal
+        self.fix_control_end_dt = fix_control_end_dt if fix_control_end_dt is not None else \
+            self.data_in.index.max()
 
         assert target_col in data_in.columns, "Column not found in input dataframe!"
         assert seasonal in [None, 24, 7, 12, 4], "seasonal parameter only accepts 4 (quarterly), 12 (monthly), " \
@@ -46,7 +58,6 @@ class SPC:
 
         # Add seasonal column to data_in and store unique seasonal periods
         if self.seasonal:
-            # What made you decide to split these functions away from the class? (add_seasonal, group by seasonal etc...)
             self.data_in['season'] = _add_seasonal_column(self.data_in, self.seasonal)  # Add season column
             self.seasonal_periods = self.data_in['season'].unique()  # Store unique seasonal values
 
@@ -54,6 +65,7 @@ class SPC:
         self.control_line_dates = {self.data_in.index.min(): {'cl_start_data': self.fix_control_start_dt,
                                                               'cl_end_data': self.fix_control_end_dt,
                                                               'process_names': None}}
+
         self.__data_blocks_list = None
         self.__process_change_dates = None
         self.__seasonal_adjustment_passed = {}
@@ -63,19 +75,29 @@ class SPC:
                                 fix_control_start_dt=None,
                                 fix_control_end_dt=None,
                                 process_name=None) -> None:
-
         """
-        Optional method, which allows control lines to be re-calculated following a systematic change to the measured
-        process. Can be called multiple times if a series of process changes occurs during the observed period.
+        Optional Method. Allows control lines to be re-calculated following a systematic change to the measured process.
+        It can be called multiple times if a series of process changes occurs during the observed period.
+
+        Inputs:
+            - change_date (str or pd.Timestamp): The date when the process change occurred.
+            - fix_control_start_dt (str or pd.Timestamp, optional): The start date for the control line calculation.
+              If None, it defaults to the change_date.
+            - fix_control_end_dt (str or pd.Timestamp, optional): The end date for the control line calculation.
+              If None, it defaults to the end of the data.
+            - process_name (str, optional): A name for the process change. This can be used to identify different process
+              changes.
         """
 
         fix_control_start_dt = fix_control_start_dt or change_date
         fix_control_end_dt = fix_control_end_dt or self.data_in.index.max()
 
-        # Default max. of previous fix_control_end_dt to change_date, if none provided, or date exceeds change_date/
+        # Default max. of previous fix_control_end_dt to change_date, if none provided, or date exceeds change_date.
         if self.fix_control_end_dt == self.data_in.index.max() or self.fix_control_end_dt > fix_control_start_dt:
-            # Dict keys are only guaranteed to be in a consistent order since Python 3.6, just something to be aware of when thinking about the package requirements
-            most_recent_key = list(self.control_line_dates.keys())[-1]  # Get previous change date
+            most_recent_key = max(
+                [pd.Timestamp(key) for key in self.control_line_dates.keys() if pd.Timestamp(key) < \
+                 pd.Timestamp(change_date)], default=None
+            )
             self.control_line_dates[most_recent_key]['cl_end_data'] = change_date  # Update end date
 
         # Add new change_date to dict.
@@ -85,25 +107,29 @@ class SPC:
 
         self.control_line_dates = convert_to_timestamp(self.control_line_dates)
 
-    def add_spc(self, spc_name, spc_calc_func, rule_1=True, rule_2=False, rule_3=False, rule_4=False, rule_5=False):
+    def calculate_spc(self, spc_name, spc_calc_func, rule_1=True, rule_2=False,
+                      rule_3=False, rule_4=False, rule_5=False) -> None:
 
         """
-        Calculates control lines (including re-calculation, if specified), tests data for control.
+        Calculates control lines (including re-calculation, if specified) & tests data for control.
 
         Inputs:
-        - spc_calc_func (function): Must return dictionary containing keys:
-            * 'process' (pandas.Series): The measured process being tracked using SPC (e.g., individuals/moving-range)
-            * 'CL' (float/int): The center/control line
-            * 'UCL' (float/int): The upper control line
-            * 'LCL' (float/int): The lower control line
-        - rule_1 (bool, default=True): Outside of control limits.
-        - rule_2 (bool, default=False): 8 (or more) consecutive points above/below center line.
-        - rule_3 (bool, default=False): 6 (or more) consecutive points increasing/decreasing.
-        - rule_4 (bool, default=False): 2 out of 3 consecutive points outside +/- 2 sigma limits
-        - rule_5 (bool, default=False): 15 (or more) consecutive points within the +/- 1 sigma limits.
 
-        Outputs:
-            - Plotly.Figure
+            - spc_name (str): The name of the SPC chart.
+            - spc_calc_func (function): A function that calculates the SPC metrics. It must return a dictionary
+              containing the following keys:
+                - 'process' (pd.Series): The measured process being tracked using SPC (e.g., individuals/moving-range).
+                - 'CL' (float or int): The center/control line.
+                - 'UCL' (float or int): The upper control line.
+                - 'LCL' (float or int): The lower control line.
+            - rule_1 (bool, optional): Rule 1 - Outside of control limits. Defaults to True.
+            - rule_2 (bool, optional): Rule 2 - 8 (or more) consecutive points above/below the center line. Defaults to
+              False.
+            - rule_3 (bool, optional): Rule 3 - 6 (or more) consecutive points increasing/decreasing. Defaults to False.
+            - rule_4 (bool, optional): Rule 4 - 2 out of 3 consecutive points outside +/- 2 sigma limits. Defaults to
+              False.
+            - rule_5 (bool, optional): Rule 5 - 15 (or more) consecutive points within the +/- 1 sigma limits. Defaults
+              to False.
         """
 
         process_change_dates = list(self.control_line_dates.keys())
@@ -117,26 +143,9 @@ class SPC:
         # Obtain list of datasets for each process_change_dates periods, if add_process_change() called.
         self.__data_blocks_list = self.__split_data_by_change_dates()
 
-        self._calculate_control_lines(spc_calc_func=spc_calc_func)
-        spc_data = self._test_for_control(rules_dict=rules_dict)
+        self.__calculate_control_lines(spc_calc_func=spc_calc_func)
 
-        self.spc_outputs_dict[spc_name] = {'data': spc_data,
-                                           'figure': plotly_chart(spc_data,
-                                                                  self.__process_change_dates,
-                                                                  self.control_line_dates,
-                                                                  figure_title=spc_name)}
-
-    def plot_spc(self, save_to_html=False, file_name='SPC Analytics Chart'):
-
-        figs = [output['figure'] for output in self.spc_outputs_dict.values()]
-        fig_names = list(self.spc_outputs_dict.keys())
-
-        return combine_figures(figures=figs,
-                               fig_names=fig_names,
-                               process_change_dates=self.__process_change_dates,
-                               process_change_dict=self.control_line_dates,
-                               save_to_html=save_to_html,
-                               file_name=file_name)
+        return self.__test_for_control(rules_dict=rules_dict)
 
     def __split_data_by_change_dates(self):
 
@@ -147,28 +156,22 @@ class SPC:
         """
 
         if not self.__process_change_dates:
-            return [self.data_in]
-        else:
+            return [self.data_in]  # No change dates, return as a single block
 
-            change_dates = sorted(self.__process_change_dates)
+        change_dates = sorted(self.__process_change_dates)
+        data_blocks_list = []  # Store data blocks in list
+        prev_date = self.data_in.index.min()  # Set previous change date to start of data
 
-            data_blocks_list = []  # Store each data "block" in list
-            prev_date = self.data_in.index.min()  # Initialise previous change date to start of data
+        for cd in change_dates:
+            data_blocks_list.append((prev_date, cd))
+            prev_date = cd  # Update prev_date to the current change date
 
-            for idx, cd in enumerate(change_dates):
-                # Do these need to be copies? I wonder if it'd be better to store slices here (see https://www.w3schools.com/python/ref_func_slice.asp)
-                block = self.data_in[(self.data_in.index >= pd.to_datetime(prev_date)) & \
-                                     (self.data_in.index < pd.to_datetime(cd))].copy()
-                data_blocks_list.append(block)
-                prev_date = cd  # Update prev_date variable
+        # Append last data block
+        data_blocks_list.append((prev_date, cd))
 
-            # Append last data block to data_blocks_list
-            final_period = self.data_in[(self.data_in.index >= pd.to_datetime(cd))].copy()
-            data_blocks_list.append(final_period)
+        return data_blocks_list
 
-            return data_blocks_list  # Store list of data block
-
-    def _calculate_control_lines(self, spc_calc_func):
+    def __calculate_control_lines(self, spc_calc_func):
 
         """
         Calculates control lines for each "block" dataset (only one dataset if no process_change_dates set).
@@ -176,16 +179,22 @@ class SPC:
 
         cl_df = pd.DataFrame()
         cl_df.index = self.data_in.index
-        cl_df[['CL', 'LCL', 'UCL', 'process', 'period']] = np.nan
+        cl_df[['CL', 'LCL', 'UCL', 'process', 'period', 'period_name']] = np.nan
+        cl_df['period'] = cl_df['period'].fillna(-1).astype(int)  # Ensure int is stored rather than float
+        cl_df['period_name'] = cl_df['period_name'].astype(object)
         if self.seasonal:
             cl_df['season'] = self.data_in['season'].values  # Add season column
 
         # Store control lines in dictionary for quick reference
         cl_dictionary = {}
 
-        for idx, df in enumerate(self.__data_blocks_list):
+        # Use slices instead of copying data frames
+        for idx, date_tuple in enumerate(self.__data_blocks_list):
 
-            # Filter data for start/end control line fixed dates
+            block_start_date, block_end_date = self.__data_blocks_list[idx]
+
+            df = self.data_in.loc[block_start_date: block_end_date]
+
             start_date_dict = self.control_line_dates.get(df.index[0], None)
             end_date_dict = self.control_line_dates.get(df.index[0], None)
 
@@ -193,22 +202,20 @@ class SPC:
                 raise ValueError(f"Warning: Control line dates not found for change date {df.index[0]}. Make sure "
                                  f"change date is of same type as index.")
 
-            else:
-                start_date = start_date_dict['cl_start_data']
-                end_date = end_date_dict['cl_end_data']
+            start_date = start_date_dict['cl_start_data']
+            end_date = end_date_dict['cl_end_data']
 
-            check_data_points_per_season = _data_points_per_season(_group_data_by_season(df.loc[start_date: end_date],
-                                                                                         self.seasonal))
+            # Check if data points per season are sufficient
+            check_data_points_per_season = _data_points_per_season(
+                _group_data_by_season(df.loc[start_date: end_date], self.seasonal))
 
             if self.seasonal and not check_data_points_per_season:
                 self.__seasonal_adjustment_passed[idx] = True
-                # This probably needs to be a warning rather than a print statement
-                print('Not enough data to build reliable estimate of mean for each season. Global mean will be '
-                      'estimated until more data is added (control line calculation period is changed, or more data '
-                      'collected)')
+                warnings.warn('Not enough data to build reliable estimate of mean for each season. Global mean will be '
+                              'estimated until more data is added (control line calculation period is changed, or '
+                              'more data collected)', UserWarning)
 
             if self.seasonal and check_data_points_per_season:
-
                 seasonal_grouped = _group_data_by_season(df, self.seasonal)
 
                 # Calculate control lines for each seasonal period
@@ -216,6 +223,7 @@ class SPC:
                     lambda group: spc_calc_func(group, self.target_col, start_date, end_date))
                 cl_dictionary[idx] = cl_dict
 
+                # Update control lines and process columns efficiently
                 cl_df.loc[df.index, 'CL'] = df['season'].map(lambda s: cl_dict[s]['CL'])
                 cl_df.loc[df.index, 'LCL'] = df['season'].map(lambda s: cl_dict[s]['LCL'])
                 cl_df.loc[df.index, 'UCL'] = df['season'].map(lambda s: cl_dict[s]['UCL'])
@@ -225,7 +233,6 @@ class SPC:
                 cl_df.loc[df.index, 'process'] = df['process'].values
 
             else:
-
                 cl_dict = spc_calc_func(df, self.target_col, start_date, end_date)
                 cl_dictionary[idx] = cl_dict
 
@@ -236,10 +243,15 @@ class SPC:
 
             cl_df.loc[df.index, 'period'] = idx
 
+        first_key = next(iter(self.control_line_dates))  # Get the first key
+        cl_df['period_name'] = cl_df.index.map(
+            lambda date: (self.control_line_dates.get(date, {}).get('process_names', np.nan)
+                          if date != first_key else np.nan))
+
         self.__cl_dict = cl_dictionary
         self.__cl_data = cl_df
 
-    def _test_for_control(self, rules_dict):
+    def __test_for_control(self, rules_dict):
 
         """
         Tests dataset for control, using the set rules.
@@ -291,4 +303,4 @@ class SPC:
 
         merged_df[list(out_of_control_dict.keys())] = merged_df[list(out_of_control_dict.keys())].fillna(0).astype(int)
 
-        return merged_df
+        return merged_df.dropna(axis=1, how='all').drop(columns=['period'])
